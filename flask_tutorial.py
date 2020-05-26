c59651e49fb668e7794ca942a1d87c9709c7ef8d
Ilhan Karadeniz <ilhany.karadeniz@gmail.com>
2020-05-27 02:44:07 +0300

    more clean code

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-#app = Flask(__name__)
 
-
-'''posts = [
-        {
-        "name":"John",
-        "date":"13.3.2020",
-        "topic":"Coronavirus school shutdowns",
-        },
-    ]'''
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
